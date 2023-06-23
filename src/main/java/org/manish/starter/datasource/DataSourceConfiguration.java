edd822457701009c18d680ad3e240ed0278d6e30
Manish Mrinal <manishmrinal@Manishs-MacBook-Pro.local>
2023-06-23 16:28:25 +0530

    First commit

---
+++ src/main/java/org/manish/starter/datasource/DataSourceConfiguration.java
@@ -0,0 +1,23 @@
+package org.manish.starter.datasource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+
+        @Bean
+        public DataSource dataSource(){
+            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            dataSource.setDriverClassName("org.h2.Driver");
+            dataSource.setUrl("jdbc:h2:mem:testdb");
+            dataSource.setUsername( "username" );
+            dataSource.setPassword( "password" );
+            return dataSource;
+        }
+
+    }
+
